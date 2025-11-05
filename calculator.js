a27969070a3bf30324e0943728c0786330e10479
Ghada31-alghamdi <s202269300@kfupm.edu.sa>
2025-11-05 12:01:21 +0300

    checked and fixed errors

--- calculator.js
+++ calculator.js
@@ -175,14 +175,14 @@ After completing all TODOs, test your calculator:
 
 */
 
-import { add, subtract } from "./utils/operations.js";
+import { add, subtract,multiply,divide } from "./utils/operations.js";
 import { parseNumbers, isValidOperation } from "./utils/parser.js";
 import _ from "lodash";
 const operation = process.argv[2];
 const numbers = process.argv.slice(3);
 if (!isValidOperation(operation)) {
     console.log("Invalid operation. Use: add, subtract, multiply, or divide");
-    return;
+    process.exit(1);
   }
   const nums = parseNumbers(numbers);
   let result;
--- utils/operations.js
+++ utils/operations.js
@@ -1,17 +1,17 @@
 
 export function add(numbers) {
-  return numbers.reduce((sum, num) => sum + num, 0);
+  return numbers.reduce((sum, num) => sum + num, 0)
 }
 
 export function subtract(numbers) {
-  return numbers.reduce((dif, num) => dif - num, 1);
+  return numbers.slice(1).reduce((dif, num) => dif - num, numbers[0])
 }
 
 export function multiply(numbers) {
-  return numbers.reduce((mul, num) => mul * num, 0)
+  return numbers.reduce((mul, num) => mul * num, 1)
 }
 
 export function divide(numbers) {
-  return numbers.reduce((div, num) => div/num, 1)
+  return numbers.slice(1).reduce((div, num) => div/num, numbers[0])
 }
 
